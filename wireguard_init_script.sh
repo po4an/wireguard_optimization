8cf67ceb58774252170317424041b60d78d91df8
GrigoryBezruchenko <grisha.bezruchenko@gmail.com>
2022-04-21 23:15:40 +0300

    add parse wg-quick status output

--- wireguard_init_script.sh
+++ wireguard_init_script.sh
@@ -34,16 +34,25 @@ if [ -z $wireguard_path ]; then
 	sysctl -p
 
 	# Run the wireguard service and check its status
-	# reminder: parse status from status command output
 	systemctl enable wg-quick@wg0.service
 	systemctl start wg-quick@wg0.service
-	systemctl status wg-quick@wg0.service
+	if [ `systemctl status wg-quick@wg0.service | awk '$1=="Active:" {print tolower($2)}'` == "active" ]; then
+		echo "The Wireguard service has active status"
+	else
+		echo "The Wireguard service has inactive status"
+	fi
 
 	# Create a metadata file that contains information about internal addresses. And add there the data about the first interface address
 	touch $addresses_data_path
 	echo $tech_user_name' '$tech_user_seq_num' '`cat $priv_key_path`' '`cat $pub_key_path` >> $addresses_data_path
 	chmod 600 $addresses_data_path
+	# reminder: add loop logic
+	new_user_adding_answer="null"
+	while [ new_user_adding_answer not in ]
+	read -p "Do you wand to add the first wireguard user right now?" new_user_adding_answer
 
+	echo "Let's not add the first user"
+	sh add_user.sh
 	# reminder: add the add_user.sh script executing
 else
 	echo "Wireguard is not installed
