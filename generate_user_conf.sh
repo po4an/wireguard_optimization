7f636b813ceaf14acb8ae428a3974145c176ab13
GrigoryBezruchenko <grisha.bezruchenko@gmail.com>
2022-04-27 22:41:29 +0300

    Completed all the files before testing

--- generate_user_conf.sh
+++ generate_user_conf.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
 wireguard_path=`whereis wireguard | awk -F ": " '{print $2}'`
 tech_user_name='main_interface'
 addresses_data_path=$wireguard_path'/addresses_data.txt'
+config_path="/home/$USER/wireguard_configs"
 all_users=`cat $addresses_data_path | awk -v tech_user_name="$tech_user_name" '$1!=tech_user_name {print$1}'`
 all_users_with_delimiter=`echo $all_users | xargs -o | tr -s ' ' '|'`
+
+# Write the first argument to the user_name variable. If the argument is not given, specify for which user the configuration file should be made.
+# Validating whether there is such a user in the metadata
 user_name=$1
-
-
 if [ -z $user_name ]; then
 	while [ `echo $user_name | grep -E -ie '^('+$all_users_with_delimiter+')$' | wc -w` != 1 ]; do
 		echo "List of active users: $all_users"
@@ -13,12 +16,11 @@ if [ -z $user_name ]; then
 	done
 fi
 
-
 user_seq_num=`cat $addresses_data_path | awk -v user_name="$user_name" '$1==user_name {print$2}'`
 user_priv_key=`cat $addresses_data_path | awk -v user_name="$user_name" '$1==user_name {print$3}'`
 main_pub_key=`cat $addresses_data_path | awk -v user_name="$tech_user_name" '$1==user_name {print$4}'`
 
-
+# Defining the contents of the configuration file
 new_user_config_text="\n[Interface]
 \nPrivateKey = $user_priv_key
 \nAddress = 10.0.0.$user_seq_num/32
@@ -30,5 +32,13 @@ new_user_config_text="\n[Interface]
 \nAllowedIPs = 0.0.0.0/0
 \nPersistentKeepalive = 20"
 
-#reminder: add genereate file
-#reminder: test it
+# Create a folder with all the configuration files, if it does not exist
+mkdir -p $config_path
+
+# Create a configuration file for the selected user and restrict access to it
+conf_file_path=$config_path"/"$user_name".conf"
+echo $new_user_config_text > $conf_file_path
+chmod 600 $conf_file_path
+
+echo "Successful! The configuration file was created and is located at the path: $conf_file_path
+Use it on the device where you want to use VPN"
